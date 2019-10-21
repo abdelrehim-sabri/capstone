061865f8d198730968e2f64d86761bc3dced8857
abdelrehim-sabri <56098203+abdelrehim-sabri@users.noreply.github.com>
2019-10-21 16:40:58 -0400

    add knn model

--- R/machine_learning.R
+++ R/machine_learning.R
@@ -1,14 +1,33 @@
+#install.packages("rpart")
+#install.packages("caret")
+#install.packages("e1071")
 #install.packages("randomForest")
+#install.packages("corrplot")
+
+
 library(randomForest)
 library(dplyr)
+library(rpart)
+library(caret)
+library(e1071)
+library(corrplot)
 
-patients = select(patients, 2, 4:15)
+#exclude the patient id from analysis
+patients = select(patients, 2, 5:9,12:15)
 head(patients)
+
+
+# Convert all column types to factor
+# col_names <- names(patients)
+# patients[,col_names] <- lapply(patients[,col_names] , factor)
+ 
+col_names <- names(patients)
+#col_names <- c('zipcode','age')
+patients[,col_names] <- lapply(patients[,col_names] , factor)
 
 str(patients)
 
 summary(patients)
-
 
 set.seed(100)
 
@@ -20,26 +39,37 @@ ValidSet <- patients[-train,]
 summary(TrainSet)
 summary(ValidSet)
 
-model1 <- randomForest(disease ~ ., data = TrainSet, importance = TRUE)
+model1 <- randomForest(employment_status ~ ., data = TrainSet, importance = TRUE)
 model1
+#randomForest(employment_status ~ gender+marital_status+children+ancestry+avg_commute+daily_internet_use+available_vehicles+disease+age  , data = TrainSet, importance = TRUE)
+#randomForest(employment_status ~ gender+ancestry+marital_status+children+disease+age+available_vehicles+education  , data = TrainSet, importance = TRUE)
+
+
+
+#randomForest(ancestry ~ age+gender+education+employment_status+children+marital_status, data = TrainSet, importance = TRUE)
+
+#logistic.model <- glm(ancestry ~ age+gender+education+employment_status, family = "binomial", data = TrainSet)
+#summary(logistic.model)
+#logistic.model <- glm(education ~ gender+zipcode+employment_status+marital_status+children+ancestry+avg_commute+daily_internet_use+available_vehicles+disease+age, family = "binomial", data = TrainSet)
+
 
 # Fine tuning parameters of Random Forest model
-model2 <- randomForest(disease ~ ., data = TrainSet, ntree = 500, mtry = 13, importance = TRUE)
+model2 <- randomForest(employment_status ~ ., data = TrainSet, ntree = 500, mtry = 3, importance = TRUE)
 model2
 
 # Predicting on train set
 predTrain <- predict(model2, TrainSet, type = "class")
 
 # Checking classification accuracy
-table(predTrain, TrainSet$disease) 
+table(predTrain, TrainSet$employment_status) 
 #model2 <- na.omit(model2) 
 
 # Predicting on Validation set
 predValid <- predict(model2, ValidSet, type = "class")
 
 # Checking classification accuracy
-mean(predValid == ValidSet$disease)                    
-table(predValid,ValidSet$disease)
+mean(predValid == ValidSet$employment_status)                    
+table(predValid,ValidSet$employment_status)
 
 
 # To check important variables
@@ -50,9 +80,9 @@ varImpPlot(model2)
 a=c()
 i=5
 for (i in 3:8) {
-  model3 <- randomForest(disease ~ ., data = TrainSet, ntree = 500, mtry = i, importance = TRUE)
+  model3 <- randomForest(employment_status ~ ., data = TrainSet, ntree = 500, mtry = i, importance = TRUE)
   predValid <- predict(model3, ValidSet, type = "class")
-  a[i-2] = mean(predValid == ValidSet$disease)
+  a[i-2] = mean(predValid == ValidSet$employment_status)
 }
 
 a
@@ -60,21 +90,34 @@ a
 plot(3:8,a)
 
 # Compare with Decision Tree
-model_dt = train(disease ~ ., data = TrainSet, method = "rpart")
+model_dt = train(employment_status ~ ., data = TrainSet, method = "rpart")
 model_dt_1 = predict(model_dt, data = TrainSet)
-table(model_dt_1, TrainSet$disease)
+table(model_dt_1, TrainSet$employment_status)
 
-mean(model_dt_1 == TrainSet$disease)
+mean(model_dt_1 == TrainSet$employment_status)
 
 # Running on Validation Set
 model_dt_vs = predict(model_dt, newdata = ValidSet)
-table(model_dt_vs, ValidSet$disease)
+table(model_dt_vs, ValidSet$employment_status)
 
-mean(model_dt_vs == ValidSet$disease)
-
-
+mean(model_dt_vs == ValidSet$employment_status)
 
 
 
 
+
+
+# Compare with K-Nearest Neighbour (KNN)
+#install.packages("pROC")
+#install.packages("mlbench")
+library(pROC)
+library(mlbench)
+
+trControl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+fit <- train(employment_status ~ ., data = TrainSet, method = "knn", tuneLength = 20, trControl = trControl)
+fit
+plot(fit)
+varImp(fit)
+pred <- predict(fit, newdata = ValidSet)
+confusionMatrix(pred, ValidSet$employment_status)
 
